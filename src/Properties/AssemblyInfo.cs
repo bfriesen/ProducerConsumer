64335db67c730c906b8ea3d75f08fe46b6900a3f
Brian Friesen <brian.friesen@gmail.com>
2013-10-14 21:07:33 -0400

    Remove unnecessary folder and move everything up.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RandomSkunk.ProducerConsumer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An implementation of the producer-consumer pattern as a single generic class.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brian Friesen")]
 [assembly: AssemblyProduct("RandomSkunk.ProducerConsumer")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
