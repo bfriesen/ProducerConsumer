b2f8ab68d25ffad873218bf0ae39f7a8f351c700
Brian Friesen <brian.friesen@gmail.com>
2013-10-14 21:44:25 -0400

    Add .nuspec file and update version number.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RandomSkunk.ProducerConsumer")]
+[assembly: AssemblyTitle("Generic Producer Consumer")]
 [assembly: AssemblyDescription("An implementation of the producer-consumer pattern as a single generic class.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brian Friesen")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.9.0")]
-[assembly: AssemblyFileVersion("0.0.9.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
